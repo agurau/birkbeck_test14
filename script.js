ab84199169940a5ae4025403aea846a624410c6f
Andrea Gurau <andrea_gurau@hotmail.com>
2021-07-01 18:00:39 +0100

    fourth commit

---
+++ script.js
@@ -0,0 +1,64 @@
+
+// let products = [
+//   {name:"Kit Kat Strawberry Cake", image:"kit-kat-strawberry-reduced.jpg", price:85},
+//   {name:"Carrot Cherry Cake", image:"carrot-cake-cherry-reduced.jpg", price:65},
+//   {name:"Kinder Bueno White Chocolate Cake", image:"kinder-bueno-white-chocolate-reduced.jpg", price:75},
+//   {name:"Princess Cake", image:"princess-cake-reduced.jpg", price:45},
+//   {name:"Kit Kat Strawberry Cake", image:"kit-kat-strawberry-reduced.jpg", price:85},
+//   {name:"Carrot Cherry Cake", image:"carrot-cake-cherry-reduced.jpg", price:65},
+//   {name:"Kinder Bueno White Chocolate Cake", image:"kinder-bueno-white-chocolate-reduced.jpg", price:75},
+//   {name:"Princess Cake", image:"princess-cake-reduced.jpg", price:45},
+//   {name:"Kit Kat Strawberry Cake", image:"kit-kat-strawberry-reduced.jpg", price:85},
+// ];
+
+// let cart = []
+
+
+// function load() {
+//   products.forEach((i, index)=> {
+      
+//       let item = document.createElement('div');
+//       item.innerHTML = `
+//       <div class="product">
+//       <img src="images/${i.image}">
+//       <p class="name">${i.name}</p>
+//       <p class="price">£${i.price}</p>
+//       <button class="add">Add to cart</button>
+//       </div>
+//       `
+//       const items = document.getElementById('items');
+
+//       item.getElementsByClassName('add')[0].addEventListener('click', ()=>{
+//           addTocart(index)
+//       });
+
+//       items.append(item)
+//   });
+// }
+
+// load();
+
+// function addTocart(index){
+//   cart.push(index)
+//   localStorage.setItem('cart', JSON.stringify(cart))
+//   alert(`${products[index].name} added to cart`)
+// }
+
+/*Target all the products*/
+let carts = document.querySelectorAll('.add-cart');
+
+/*Every time button add to cart is clicked we want to do something
+We gonna loop through all of them. Our cart now has a lengh of 9, so it will go from 0 to 8*/
+for (let i=0; i < carts.length; i++) {
+  // console.log("my loop")
+  /*I grab all my carts based on this formula, i will go from 0 to 8*/
+  carts[i].addEventListener('click', () => {
+    // console.log("added to cart")
+    cartNumbers();
+  })
+}
+
+/*To know how many items are added to the cart */
+function cartNumbers() {
+  localStorage.setItem('cartNumbers', 1);
+}
